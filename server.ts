42fba9b19d70fe59f139971aef94caac7252e3cb
Vlad Ra <vlad19@gmail.com>
2023-03-15 20:45:21 +0000

    server version upd

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { Hono } from "https://deno.land/x/hono/mod.ts";
 import { cors } from "https://deno.land/x/hono/middleware.ts";
 
-import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
 import { bot } from "./bot.ts";
 import { webhookCallback } from "./deps.deno.ts";
 
