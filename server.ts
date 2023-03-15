e9ed03d69ee4759207833a6f7765a1d32a5080ee
Vlad Ra <vlad19@gmail.com>
2023-03-15 12:17:04 +0000

    adding hono

--- server.ts
+++ server.ts
@@ -1,19 +1,18 @@
+import { Hono } from "https://deno.land/x/hono/mod.ts";
+import { cors } from "https://deno.land/x/hono/middleware.ts";
+
 import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 import { bot } from "./bot.ts";
 import { webhookCallback } from "./deps.deno.ts";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
-serve(async (req) => {
-  if (req.method == "POST") {
-    const url = new URL(req.url);
-    if (url.pathname.slice(1) == bot.token) {
-      try {
-        return await handleUpdate(req);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  }
-  return new Response();
+const app = new Hono();
+
+app.get("/", (c) => c.text("Hello Deno!"));
+
+app.post("/telegram", () => {
+  return await handleUpdate(req);
 });
+
+serve(app.fetch);
